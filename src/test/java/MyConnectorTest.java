6135d97672ecf0ae4c5f3235f230c6f6e708f522
Abhishek Tiwari <abhi.007twr@gmail.com>
2023-10-04 22:40:19 +0530

    updating junit

--- src/test/java/MyConnectorTest.java
+++ src/test/java/MyConnectorTest.java
@@ -3,11 +3,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 
+@ExtendWith(MockitoExtension.class)
 class MyConnectorTest {
-    MyConnector myConnector = new MyConnector();
+
+    @InjectMocks
+    MyConnector myConnector;
 
     @Test
     void testFetchDetailsByName() throws IOException, InterruptedException {
